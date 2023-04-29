7cbad6c1728f77fc2088d14f200de21ea0797961
sachetwasti1997 <sachetwasti61@gmail.com>
2023-04-29 12:10:25 +0530

    Add Event Bus

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ app.post('/events', async (req, res) => {
     await axios.post("http://posts-clusterip-srv:4000/events", event).catch(err => {
         console.log(err.message);
     });
-    // await axios.post(BASE_URL+"4001"+EVENTS_ENDPOINT, event).catch(err => {
-    //     console.log(err.message);
-    // });
-    // await axios.post(BASE_URL+"4002"+EVENTS_ENDPOINT, event).catch(err => {
-    //     console.log(err.message);
-    // });
-    // await axios.post(BASE_URL+"4003"+EVENTS_ENDPOINT, event).catch(err => {
-    //     console.log(err.message);
-    // });
+    await axios.post("http://comments-srv:4001/events", event).catch(err => {
+        console.log(err.message);
+    });
+    await axios.post("http://query-srv:4002/events", event).catch(err => {
+        console.log(err.message);
+    });
+    await axios.post("http://moderation-srv:4003/events", event).catch(err => {
+        console.log(err.message);
+    });
 
     res.status(200).send({"status": "OK"});
 });
